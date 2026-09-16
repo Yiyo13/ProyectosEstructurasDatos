e6eec0e85aae9148a0b948cca0912217f943f3f7
Yiyo <geovanny.obando.olivas@est.una.ac.cr>
2026-09-15 22:18:15 -0600

    clase pasajero(falta la lista enlazada)

---
+++ src/com/proyecto/mvc/models/Passenger.java
@@ -0,0 +1,55 @@
+package com.proyecto.mvc.models;
+
+public class Passenger {
+	
+	private int idP;
+	private String nameP;
+	private int ageP;
+	
+	
+	public Passenger(int idP, String nameP, int ageP) {
+		
+		this.idP = idP;
+		this.nameP = nameP;
+		this.ageP = ageP;
+	}
+
+
+	public int getIdP() {
+		return idP;
+	}
+
+
+	public void setIdP(int idP) {
+		this.idP = idP;
+	}
+
+
+	public String getNameP() {
+		return nameP;
+	}
+
+
+	public void setNameP(String nameP) {
+		this.nameP = nameP;
+	}
+
+
+	public int getAgeP() {
+		return ageP;
+	}
+
+
+	public void setAgeP(int ageP) {
+		this.ageP = ageP;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Passenger [idP=" + idP + ", nameP=" + nameP + ", ageP=" + ageP + "]";
+	}
+	
+	
+
+}
